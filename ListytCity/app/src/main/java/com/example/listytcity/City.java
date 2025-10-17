a50e09215c5fb57badbeed89eb2899b2deffedef
bsong718 <bsong2@ualberta.ca>
2025-10-16 19:06:01 -0600

    4

--- ListytCity/app/src/main/java/com/example/listytcity/City.java
+++ ListytCity/app/src/main/java/com/example/listytcity/City.java
@@ -16,6 +16,12 @@ public class City implements Comparable<City>{
     String getProvinceName(){
         return this.province;
     }
+
+    /**
+    * Compares this city object to another city for ordering.
+    * @param o
+    * @return a integer as this city's name to compare to the specified city's name
+    */
     @Override
     public int compareTo(City o) {
         City city = (City) o;
